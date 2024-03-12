4fe6b3ebcccc950032d5c4fd571028e9f6ba4a86
AllanKenzo <allan.takose@gmail.com>
2024-03-12 08:48:40 -0300

    primeira implementacao soma mult subtr divisao

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+def soma(x,y):
+    return x + y
+
+def subtracao(x,y):
+    return x - y
+
+def multiplicacao(x,y):
+    return x * y
+
+def divisao(x,y):
+    return x / y
+
+
+def main():
+    x = float(input("Digite o primeiro número: "))
+    y = float(input("Digite o segundo número: "))
+    op = int(input("Escolha a operação:\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão"))
+    aux = 1
+    while 
+    if op == 1:
+        res = soma(x,y)
+        print("A soma de ",x," e ",y," é: ", res)
